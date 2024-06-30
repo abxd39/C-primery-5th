ebb73731f3a7fa42b8f45ea3d5753f9498007c3c
abxd39 <764137119@qq.com>
2024-06-30 10:50:11 +0800

    feat:类

--- CppPrimer-master/ch14/ex14_02.cpp
+++ CppPrimer-master/ch14/ex14_02.cpp
@@ -26,10 +26,10 @@ std::istream& operator>>(std::istream &is, Sales_data &item)
 {
     double price = 0.0;
     is >> item.bookNo >> item.units_sold >> price;
-    if (is)
+    if (is)//检查是否输入成功
         item.revenue = price * item.units_sold;
     else
-        item = Sales_data();
+        item = Sales_data();//输入失败。对象被赋予默认的状态
     return is;
 }
 
